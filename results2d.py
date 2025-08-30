0870ad2f2edd066ffb078813881f184cd5aa9b6e
alannakamura <alancosta1988@gmail.com>
2025-08-30 18:46:36 -0300

    300825

--- results2d.py
+++ results2d.py
@@ -9,7 +9,7 @@ from optimisationMap import *
 import pickle
 
 # name_file = 'results.pkl'
-name_file = 'results_4_100sim_100iter_128pop_3posdim_1.0alpha_5070_Ti.pkl'
+name_file = 'results_32_10sim_100iter_128pop_3posdim_2.0alpha_3060.pkl'
 
 f = open(name_file, 'rb')
 results = pickle.load(f)
--- results3d.py
+++ results3d.py
@@ -13,7 +13,7 @@ from optimisationMap import *
 # name_file = 'testes/220725/versao1/results_2_100sim_30iter_128pop_3posdim_1.0alpha_3060.pkl'
 # name_file = 'testes/220725/versao2/results_2_100sim_30iter_128pop_3posdim_1.0alpha_3060.pkl'
 # name_file = 'results_1_10sim_50iter_256pop_3posdim_1.0alpha_3060.pkl'
-name_file = 'results_4_100sim_100iter_128pop_3posdim_1.0alpha_5070_Ti.pkl'
+name_file = 'results_4_100sim_50iter_128pop_3posdim_1.0alpha_5070_Ti.pkl'
 
 f = open(name_file, 'rb')
 results = pickle.load(f)
