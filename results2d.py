7ff34d4738a14e20171dd9abe0d04048bc495b5c
alannakamura <alancosta1988@gmail.com>
2025-08-31 22:57:59 -0300

    310825

--- results2d.py
+++ results2d.py
@@ -9,7 +9,7 @@ from optimisationMap import *
 import pickle
 
 # name_file = 'results.pkl'
-name_file = 'results_37_200sim_50iter_128pop_3posdim_2.0alpha_3060.pkl'
+name_file = 'results_36_300sim_50iter_128pop_3posdim_2.0alpha_5070_Ti.pkl'
 
 f = open(name_file, 'rb')
 results = pickle.load(f)
--- runMany_tests2.py
+++ runMany_tests2.py
@@ -3,8 +3,8 @@ from tqdm import tqdm
 import os
 import pycuda.driver as cuda
 
-# problem = [36]
-problem = [31,32,33,35,36,37]
+problem = [36]
+# problem = [31,32,33,35,36,37]
 # problem = [11,12,13,14,16]
 # problem = [4,7,1,2,3,5,6]
 #problem = [31, 32, 33, 35, 36, 37]
@@ -18,7 +18,7 @@ for j in range(len(problem)):
 
     num = 300
     iterations = 50
-    population = 128
+    population = 256
     pos_dim = 3
     f = open('results.pkl', 'wb')
     results = {'count': -1, 'cpu': [], 'gpu': [], 'problem': problem[j],
